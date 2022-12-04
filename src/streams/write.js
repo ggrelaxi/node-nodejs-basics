18a04eea8f3d81392882371a4458abd1a708801a
Alex Smirnov <ggrelaxi@gmail.com>
2022-12-04 23:32:01 +0300

    task 1. Gzip and streams. have done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+import * as url from "node:url";
+import path from "node:path";
+import { stdin } from "node:process";
+
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+    const destinationFile = path.resolve(__dirname, "files", "fileToWrite.txt");
+    const writableStream = fs.createWriteStream(destinationFile);
+    stdin.on("data", (chunk) => {
+        writableStream.write(chunk.toString());
+    });
 };
 
 await write();

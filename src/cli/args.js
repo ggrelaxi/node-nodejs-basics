90b10319be04804484926e521755aad67c31272d
Alex Smirnov <ggrelaxi@gmail.com>
2024-10-07 22:57:10 +0300

    finish cli task

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
+import process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+    const [environmentPath, filepath, ...userArgv] = process.argv
+    let parts = []
+
+    for (let i = 0; i < userArgv.length; i += 2) {
+        parts.push(`${userArgv[i].slice(2)} is ${userArgv[i + 1]}`);
+    }
+
+    console.log(parts.join(', '))
 };
 
 parseArgs();

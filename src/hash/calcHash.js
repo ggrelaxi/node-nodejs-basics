0802a72170f1f8d0327e1a736a33cbfaf8e35b6f
Alex Smirnov <ggrelaxi@gmail.com>
2024-10-07 23:11:58 +0300

    finish hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { resolve } from 'node:path';
+import { createReadStream } from 'node:fs';
+import { createHash } from 'node:crypto';
+import { stdout } from 'node:process';
+
 const calculateHash = async () => {
     // Write your code here 
+
+    const hash = createHash('sha256')
+    const streamData = createReadStream(resolve(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt'))
+
+    streamData.pipe(hash).setEncoding('hex').pipe(stdout)
 };
 
 await calculateHash();

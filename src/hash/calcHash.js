80bcaab6fd7b1101f94bc3a541ab91890c307e89
aleksandr.smirnov <ggrelaxi@gmail.com>
2025-04-24 00:24:58 +0300

    finish hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import fs from 'node:fs';
+import path from 'node:path';
+import crypto from 'node:crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+	// Write your code here
+	const sourceFilepath = path.resolve(import.meta.dirname, 'files', 'fileToCalculateHashFor.txt');
+	const readStream = fs.createReadStream(sourceFilepath);
+	const hash = crypto.createHash('sha256');
+
+	readStream
+		.on('data', (chunk) => {
+			hash.update(chunk);
+		})
+		.on('end', () => {
+			console.log(hash.digest('hex'));
+		});
 };
 
 await calculateHash();

87b69dbbd294140c36bbe850f8ce3d0ce86b1e44
aleksandr.smirnov <ggrelaxi@gmail.com>
2025-04-23 22:18:52 +0300

    finish fs tasks

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'node:path';
+
 const create = async () => {
-    // Write your code here 
+	// Write your code here
+	const filepath = path.resolve(import.meta.dirname, 'files', 'fresh.txt');
+
+	const isExists = await fs
+		.access(filepath)
+		.then(() => true)
+		.catch(() => false);
+
+	if (isExists) {
+		throw new Error('FS operation failed');
+	}
+
+	try {
+		await fs.writeFile(filepath, 'I am fresh and young');
+	} catch {
+		throw new Error('FS operation failed');
+	}
 };
 
 await create();
--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import path from 'node:path';
+
 const remove = async () => {
-    // Write your code here 
+	// Write your code here
+	const sourceFilepath = path.resolve(import.meta.dirname, 'files', 'fileToRemove.txt');
+
+	const isSourceFileExists = await fs
+		.access(sourceFilepath)
+		.then(() => true)
+		.catch(() => false);
+
+	if (!isSourceFileExists) {
+		throw new Error('FS operation failed');
+	}
+
+	try {
+		await fs.rm(sourceFilepath);
+	} catch {
+		throw new Error('FS operation failed');
+	}
 };
 
 await remove();

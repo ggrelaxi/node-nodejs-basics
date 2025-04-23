07dd41fa346db6c62a7f56afd43361b634430eb8
aleksandr.smirnov <ggrelaxi@gmail.com>
2025-04-23 22:37:39 +0300

    finish cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import process from 'node:process';
+
 const parseArgs = () => {
-    // Write your code here 
+	// Write your code here
+	const args = process.argv.slice(2);
+	const content = [];
+
+	for (let i = 0; i < args.length; i += 2) {
+		const name = args[i];
+		const value = args[i + 1];
+
+		content.push(`${name} is ${value}`);
+	}
+
+	console.log(content.join(', '));
 };
 
 parseArgs();

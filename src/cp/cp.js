5d0682f67482e117838218666d2c97ffb9f50905
aleksandr.smirnov <ggrelaxi@gmail.com>
2025-04-24 14:40:56 +0300

    finish cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
+import { spawn, exec, fork } from 'node:child_process';
+import path from 'node:path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	// Write your code here
+	const sourceScriptPath = path.resolve(import.meta.dirname, 'files', 'script.js');
+	fork(sourceScriptPath, args, {
+		stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+	});
 };
 
 spawnChildProcess();
